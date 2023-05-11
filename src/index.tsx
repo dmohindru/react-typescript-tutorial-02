279035557aacafee996950382e8077eb0f120b9d
Dhruv Mohindru <dmohindru@gmail.com>
2023-05-12 07:00:44 +1000

    Added an example of class based component

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 // 1) Import ReactDOM library
 import ReactDOM from "react-dom/client";
-import UserSearch from "./refs/UserSearch";
+import UserSearch from "./classes/UserSearch";
  
 // 2) Get a reference to the div with ID root
 const el = document.getElementById("root");
@@ -10,9 +10,15 @@ const root = ReactDOM.createRoot(el!);
  
 // 4) Create a component
 const App = () => {
+  const myusers = [
+    {name: 'dhruv', age: 40},
+    {name: 'poonam', age: 30},
+    {name: 'daksh', age: 13},
+    {name: 'parth', age: 11}
+  ]
   return (
     <div>
-      <UserSearch />
+      <UserSearch users={myusers} />
     </div>
   );
 };
